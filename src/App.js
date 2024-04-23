ca0154aee6fbadca8f981b567e647a83df8fcfa9
rajaniyadav9969 <88242488+rajaniyadav9969@users.noreply.github.com>
2024-04-23 09:37:44 +0530

    fixed scrolltotop  button

--- src/App.js
+++ src/App.js
@@ -7,41 +7,40 @@ import CardDeal from './Components/CardDeal/CardDeal';
 import Client from './Components/Client/Client';
 import Footer from './Components/Footer/Footer';
 import "bootstrap/dist/css/bootstrap.min.css"
+import { FaCircleArrowUp } from "react-icons/fa6";
 import 'aos/dist/aos.css';
 import AOS from 'aos';
 
 function App() {
   useEffect(() => {
-    AOS.init({once: false});
-    setTimeout(()=>{
+    AOS.init({ once: false });
+    setTimeout(() => {
       AOS.refresh();
     }, 1500)
   }, []);
+
+ const  scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  };
   return (
     <div className="App " style={{}}>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          // target="_blank"
-          // rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
       <NavBar />
       <Home />
+      <div className='scrollToTop' >
+        <button onClick={scrollToTop}><FaCircleArrowUp /></button>
+      </div>
+
       <div className="containers">
         <FeatureComp />
-        <Product/>
-        <CardDeal/>
-        <Client/>
-        <Footer/>
+        <Product />
+        <CardDeal />
+        <Client />
+        <Footer />
       </div>
+
     </div>
   );
 }
